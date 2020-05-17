ad3446c64f6cdd29d43ca3280076276d32264897
Mahathir <del_xmen@yahoo.com>
2020-05-17 22:03:18 +0800

    commit

--- order-service/src/orm.config.ts
+++ order-service/src/orm.config.ts
@@ -1,12 +1,19 @@
 import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot({
+    envFilePath: 'development.env',
+});
 
 export const config: TypeOrmModuleOptions = {
-    type: 'postgres',
-    port: 5432,
-    username: "postgres",
-    password: "123",
+    type: 'mysql',
+    port: 3308,
+    username: "root",
+    password: process.env.DATABASE_PASSWORD,
     database: "orderapp",
     host: '127.0.0.1',
     entities: ["dist/**/*.entity{.ts,.js}"],
-    synchronize: true
+    synchronize: true,
+    retryAttempts: 100,
+    retryDelay: 5000
 }
